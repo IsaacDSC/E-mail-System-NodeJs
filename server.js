53fc7e4647e46973c285d72e17bcb780f68a30c2
IsaacDSC <56350331+IsaacDSC@users.noreply.github.com>
2020-08-20 11:30:08 -0300

    Update server.js
    segurança

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const nodemailer = require('nodemailer')
 let transporter = nodemailer.createTransport({
     service: 'Hotmail',
     auth: {
-        user: 'isaac8.silva@hotmail.com',
-        pass: 'RAISSAsenha'
+        user: 'nome@hotmail.com',
+        pass: '*****'
     }
 })
 
 transporter.sendMail({
-    from: 'Isaac Dsc <isaac8.silva@hotmail.com>',
-    to: 'isaacdsc10@gmail.com',
+    from: 'Isaac Dsc <nome@hotmail.com>',
+    to: 'nome@gmail.com',
     subject: 'Ola, sou Isaac e estou trabalhando com nodemailer',
     text: 'Olá estou gostando bastante da lib nodemailer',
     html: 'Para mais info:<a href="https://www.google.com/search?q=google&rlz=1C1GCEB_enBR891BR891&oq=goog&aqs=chrome.0.69i59j69i57j69i59j69i60l2j69i65l3.471j0j7&sourceid=chrome&ie=UTF-8">Pesquise no google</a>'
